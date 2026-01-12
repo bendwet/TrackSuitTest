8d27d6bb9e8d3e57eb452f2df05b19587d8b8570
bendwet <bendwet@outlook.com>
2026-01-13 09:22:16 +1300

    Add create

---
+++ server/operations/create-insights.ts
@@ -0,0 +1,33 @@
+import type { Insight } from "$models/insight.ts";
+import type { HasDBClient } from "../shared.ts";
+import type * as insightsTable from "$tables/insights.ts";
+
+type Input = HasDBClient & {
+  brand: number;
+  insight: string;
+};
+
+export default (input: Input): Insight => {
+  console.log("Creating insight");
+      console.log(input.brand, input.insight);  
+  
+  const createdAt = new Date().toISOString();
+
+  const [row] = input.db
+    .sql<
+    insightsTable.Row
+  >`INSERT INTO insights (brand, createdAt, text)
+    VALUES (${input.brand}, ${createdAt}, ${input.insight})
+    RETURNING *`;
+
+  if (!row) {
+    throw new Error("Failed to create insight");
+  }
+
+  const result: Insight = {
+    ...row,
+    createdAt: new Date(row.createdAt),
+  };
+  console.log("Insight created:", result);
+  return result;
+};
